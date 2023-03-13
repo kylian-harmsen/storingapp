94e93ee03ed4ea58f6165e5e90a7d00bda1d2cdf
kylian-harmsen <D296238@edu.curio.nl>
2023-03-13 08:55:34 +0100

    veranderingen

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -2,6 +2,7 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
+$type = $_POST['type']
 $capaciteit = $_POST['capaciteit']; 
 $melder = $_POST['melder'];
 
@@ -11,7 +12,13 @@ echo $attractie . " / " . $capaciteit . " / " . $melder;
 require_once 'conn.php';
 
 //2. Query
-
+$query = "INSERT INTO meldingen (attractie, type)
+VALUES(:attractie, :type)";
 //3. Prepare
-
+$statement = $conn->prepare($query);
+$statement->execute([
+    ":attractie" => $attractie,
+    ":type" => $type,
+]);
 //4. Execute
+$items = $statement->fetchAll(PDO::FETCH_ASSOC);
