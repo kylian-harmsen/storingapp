e128f7bd5925dbe1d002423744a5cae67ededdbe
kylian-harmsen <D296238@edu.curio.nl>
2023-03-14 09:46:22 +0100

    final build

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -2,8 +2,20 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
+if(empty($attractie))
+{
+    $errors[] = "Vul de attractie-naam in.";
+}
 $type = $_POST['type'];
+if(empty($type))
+{
+    $errors[] = "Vul type in.";
+}
 $capaciteit = $_POST['capaciteit']; 
+if(!is_numeric($capaciteit))
+{
+    $errors [] = "vul een getal in";
+}
 if(isset($_POST['prioriteit']))
 {
     $prioriteit = true;
@@ -13,16 +25,24 @@ else
     $prioriteit = false;
 }
 $melder = $_POST['melder'];
+if(empty($melder))
+{
+    $errors[] = "Vul een melding in.";
+}
 $overig = $_POST['overig'];
-
+if(isset($errors))
+{
+    var_dump($errors);
+    die();
+}
 echo $attractie . " / " . $capaciteit . " / " . $melder;
 
 //1. Verbinding
 require_once 'conn.php';
 
 //2. Query
-$query = "INSERT INTO meldingen (attractie, type, capaciteit, prioriteit, melder, overig)
-VALUES(:attractie, :type, :capaciteit, :prioriteit, :melder, :overig)";
+$query = "INSERT INTO meldingen (attractie, type, capaciteit, prioriteit, melder, overige_info)
+VALUES(:attractie, :type, :capaciteit, :prioriteit, :melder, :overige_info)";
 //3. Prepare
 $statement = $conn->prepare($query);
 //4. Execute
@@ -32,6 +52,6 @@ $statement->execute([
     ":capaciteit" => $capaciteit,
     ":prioriteit" => $prioriteit,
     ":melder" => $melder,
-    ":overig" => $overig,
+    ":overige_info" => $overig,
 ]);
-header(location:../meldingen/index.php?msg=Melding opgeslagen);
+header("location:../meldingen/index.php?msg=Melding opgeslagen");
